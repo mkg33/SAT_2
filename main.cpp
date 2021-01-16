2621af987269ae93204f583edbee3998d132c8ed
mkg33 <maja.k.gwozdz@gmail.com>
2021-01-16 16:51:44 +0100

    Added instructions and solver name.
    Feel free to pick another font :-)

--- main.cpp
+++ main.cpp
@@ -5,13 +5,32 @@
 
 int main(int argc, char **argv)
 {
-    if (argc < 3) { // 'lucky' picks the heuristic randomly
-        std::cerr << "Usage: " << argv[0] << " <DIMACS file>" << " <selection heuristics: WITHOUT, YESNO, RANDOM, DLIS, RDLIS, DLCS, RDLCS, JW, RJW, MOMS, RMOMS, lucky>\n";
+    std::string option = "without"; // default: no selection heuristic
+    if (argc < 2) { // 'ANSI shadow' font from https://www.patorjk.com/software/taag/
+        std::cout << R"(
+███╗   ███╗ █████╗ ██████╗ ██╗  ██╗███████╗ ██████╗ ██╗     ██╗   ██╗███████╗██████╗
+████╗ ████║██╔══██╗██╔══██╗██║  ██║██╔════╝██╔═══██╗██║     ██║   ██║██╔════╝██╔══██╗
+██╔████╔██║███████║██████╔╝███████║███████╗██║   ██║██║     ██║   ██║█████╗  ██████╔╝
+██║╚██╔╝██║██╔══██║██╔═══╝ ██╔══██║╚════██║██║   ██║██║     ╚██╗ ██╔╝██╔══╝  ██╔══██╗
+██║ ╚═╝ ██║██║  ██║██║     ██║  ██║███████║╚██████╔╝███████╗ ╚████╔╝ ███████╗██║  ██║
+╚═╝     ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝  ╚═╝╚══════╝ ╚═════╝ ╚══════╝  ╚═══╝  ╚══════╝╚═╝  ╚═╝
+        )";
+        std::cerr <<"\nUsage: " << argv[0] << " <DIMACS file>" << " optional selection heuristic [case insensitive]:\n <YESNO | RANDOM | DLIS | RDLIS | DLCS | RDLCS | JW | RJW | MOMS | RMOMS | lucky>\n\n"
+        << "Available selection heuristics: \n" << "- YES/NO: select the first available literal and use a random yes/no decision during the selection\n"
+        << "- RANDOM: select a random literal\n" << "- DLIS: Dynamic Largest Individual Sum\n" << "- RDLIS: randomized Dynamic Largest Individual Sum\n"
+        << "- DLCS: Dynamic Largest Combined Sum\n" << "- RDLCS: randomized Dynamic Largest Combined Sum\n" << "- JW: Jeroslow-Wang heuristic\n"
+        << "- RJW: randomized Jeroslow-Wang heuristic\n" << "- MOMS: Maximum [number of] Occurrences in Minimum [length] Clauses\n"
+        << "- RMOMS: randomized Maximum [number of] Occurrences in Minimum [length] Clauses\n" << "- lucky: select random heuristic\n\n"
+        << "Note: If no selection heuristic is specified, the first available literal is selected.\n\n";
+
         return 1;
+    }
+    else if (argc == 3) {
+        option = argv[2];
     }
 
     std::ifstream stream(argv[1]);
-    std::string option(argv[2]);
+
     Solver solver(stream, option);
 
     solver.solve();
